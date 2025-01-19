cd2b10480cc52d2fb7b682e86290f9f9f44d5a49
andro-jr <prabin20panta@gmail.com>
2025-01-19 13:14:15 +0545

    add: trend components calculations

--- pages/index.vue
+++ pages/index.vue
@@ -14,17 +14,31 @@ const selectedView = ref(transactionViewOptions[1]);
   <section
     class="grid gid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
   >
-    <TrendSection color="green" title="Income" :amount="4000" :loading="true" />
     <TrendSection
       color="green"
       title="Income"
       :amount="4000"
+      :last-amount="3000"
       :loading="false"
     />
-    <TrendSection color="red" title="Income" :amount="4000" :loading="false" />
     <TrendSection
       color="green"
-      title="Income"
+      title="Expense"
+      :amount="4000"
+      :last-amount="6000"
+      :loading="false"
+    />
+    <TrendSection
+      color="red"
+      title="Investments"
+      :amount="4000"
+      :last-amount="2000"
+      :loading="false"
+    />
+    <TrendSection
+      color="green"
+      title="Savings"
+      :last-amount="8000"
       :amount="4000"
       :loading="false"
     />
