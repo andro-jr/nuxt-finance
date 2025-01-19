24a8683c8f993c7a559159d6de4daf0a1c43ec72
andro-jr <prabin20panta@gmail.com>
2025-01-19 12:19:28 +0545

    add; summary section and period section

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { transactionViewOptions } from "~/constants.js";
+const selectedView = ref(transactionViewOptions[1]);
+</script>
+
 <template>
-  <div>Index Page</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelect v-model="selectedView" :options="transactionViewOptions" />
+    </div>
+  </section>
 </template>
