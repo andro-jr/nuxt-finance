17b08138956e8b7d2823134e52ffb0d41db0d5b2
andro-jr <prabin20panta@gmail.com>
2025-01-25 11:16:17 +0545

    chore: supabase environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/supabase']
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  supabase:{
+    redirect:false
+  }
 })
